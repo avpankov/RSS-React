354384896dfc0290fcb360e2ed5cb0ad5a9a4730
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-03-16 14:59:33 +0300

    refactor: create component ListOfCards

---
+++ src/components/listOfCards.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../components/card';
+import products from '../data/products.json';
+
+class ListOfCards extends React.Component {
+  render() {
+    return (
+      <div className="flex flex-row flex-wrap justify-between gap-6">
+        <>
+          {products.products.map((product) => {
+            return <Card product={product} key={product.id} />;
+          })}
+        </>
+      </div>
+    );
+  }
+}
+
+export default ListOfCards;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import SearchBar from '../components/searchBar';
-import Card from '../components/card';
-import products from '../data/products.json';
+import ListOfCards from '../components/listOfCards';
 
 class MainPage extends React.Component {
   render() {
     return (
       <div className="container mx-auto">
         <SearchBar />
-        <div className="flex flex-row flex-wrap justify-between gap-6">
-          <>
-            {products.products.map((product) => {
-              return <Card product={product} key={product.id} />;
-            })}
-          </>
-        </div>
+        <ListOfCards />
       </div>
     );
   }
