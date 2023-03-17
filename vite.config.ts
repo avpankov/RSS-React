f3c8c8df7e0f462c14b425f4c3721a1c54cf728a
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-03-17 09:16:24 +0300

    feat: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteSvgr from 'vite-plugin-svgr';
@@ -5,4 +8,14 @@ import viteSvgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteSvgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTest.ts'],
+    coverage: {
+      all: true,
+      provider: 'c8',
+      include: ['src/**/*.tsx'],
+    }
+  },
 });
