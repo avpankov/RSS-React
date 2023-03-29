5a8efbd4b4de7d8172b9088f9e5c9a354340a68c
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-03-29 10:47:35 +0300

    refactor: rewrite searchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,47 +1,38 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import searchIcon from '../assets/icons/search.svg';
 
-class SearchBar extends React.Component<Record<string, never>, { searchInputValue: string }> {
-  componentDidMount() {
-    const value = localStorage.getItem('searchInputValue');
-    if (!value) return;
-    if (value) this.setState({ searchInputValue: value });
-  }
+function SearchBar() {
+  const [searchInputValue, setSearchInputValue] = useState(
+    localStorage.getItem('searchInputValue') || ''
+  );
 
-  componentWillUnmount() {
-    if (!this.state) return;
-    localStorage.setItem('searchInputValue', this.state.searchInputValue);
-  }
+  useEffect(() => {
+    localStorage.setItem('searchInputValue', searchInputValue);
+  }, [searchInputValue]);
 
-  checkLocalStorage() {
-    return this.state ? this.state.searchInputValue : '';
-  }
+  const handleInputChange = (event: ChangeEvent) => {
+    setSearchInputValue((event.target as HTMLInputElement).value);
+  };
 
-  handleInputChange(event: ChangeEvent) {
-    this.setState({ searchInputValue: (event.target as HTMLInputElement).value });
-  }
-
-  render() {
-    return (
-      <div className="w-[685px] flex flex-row relative py-6 mx-auto">
-        <img
-          src={searchIcon}
-          width="20px"
-          alt="Search icon"
-          className="absolute top-[50%] translate-y-[-50%] ml-4 opacity-60"
-        />
-        <input
-          type="text"
-          value={this.checkLocalStorage()}
-          onChange={(event) => this.handleInputChange(event)}
-          name="search"
-          id="search"
-          placeholder="Search"
-          className="w-[100%] h-[48px] p-4 pl-[45px] rounded-md border border-slate-200 outline-brand"
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="w-[685px] flex flex-row relative py-6 mx-auto">
+      <img
+        src={searchIcon}
+        width="20px"
+        alt="Search icon"
+        className="absolute top-[50%] translate-y-[-50%] ml-4 opacity-60"
+      />
+      <input
+        type="text"
+        value={searchInputValue}
+        onChange={(event) => handleInputChange(event)}
+        name="search"
+        id="search"
+        placeholder="Search"
+        className="w-[100%] h-[48px] p-4 pl-[45px] rounded-md border border-slate-200 outline-brand"
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
