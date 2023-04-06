600bba53a45e818238c81f77d37937fcedc25736
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-04-06 10:22:33 +0300

    refactor: create LoadingSpinner component

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import ListOfCards, { type } from '../../components/ListOfCards/ListOfCards';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import { getProducts } from '../../api/getProducts';
 import { searchProducts } from '../../api/searchProducts';
 
@@ -37,16 +38,7 @@ function MainPage() {
     <div className="container mx-auto">
       <SearchBar onEnterPressed={handleSearch} />
       {isLoaded ? (
-        <div className="flex items-center justify-center space-x-2">
-          <div
-            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-brand motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
-          </div>
-        </div>
+        <LoadingSpinner />
       ) : products.length > 0 ? (
         <div className="flex flex-row flex-wrap justify-between gap-6">
           <ListOfCards cardType={type.short} products={products} />
