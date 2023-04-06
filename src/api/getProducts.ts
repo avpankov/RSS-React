c7d9c0b3abff2333ae15280d7f9a881102eca458
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-04-06 14:19:26 +0300

    feat: add error handling

--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'https://dummyjson.com/products';
+export const baseUrl = 'https://dummyjson.com/product';
 
 export async function getProducts() {
   const { data } = await axios.get(baseUrl);
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,12 +4,14 @@ import ListOfCards, { type } from '../../components/ListOfCards/ListOfCards';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import { getProducts } from '../../api/getProducts';
 import { searchProducts } from '../../api/searchProducts';
+import { AxiosError } from 'axios';
 
 function MainPage() {
   const [searchedValue, setSearchedValue] = useState(
     localStorage.getItem('searchInputValue') || ''
   );
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState<AxiosError | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
 
   const handleSearch = (value: string) => {
@@ -19,17 +21,31 @@ function MainPage() {
   useEffect(() => {
     if (searchedValue === '') {
       async function foo() {
-        setProducts(await getProducts());
+        try {
+          setProducts(await getProducts());
+          setError(null);
+        } catch (error: unknown) {
+          if (error instanceof AxiosError) {
+            setError(error);
+          }
+        }
       }
       foo();
     }
-  });
+  }, [searchedValue]);
 
   useEffect(() => {
     async function foo() {
-      setIsLoaded(true);
-      setProducts(await searchProducts(searchedValue));
-      setIsLoaded(false);
+      try {
+        setIsLoaded(true);
+        setProducts(await searchProducts(searchedValue));
+        setError(null);
+        setIsLoaded(false);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          setError(error);
+        }
+      }
     }
     foo();
   }, [searchedValue]);
@@ -37,16 +53,23 @@ function MainPage() {
   return (
     <div className="container mx-auto">
       <SearchBar onEnterPressed={handleSearch} />
-      {isLoaded ? (
+      {error ? (
+        <div className="text-center">
+          <h2 className="font-semibold text-2xl">{error.message}</h2>
+          {(error.response?.data as AxiosError).message && (
+            <p>{(error.response?.data as AxiosError).message}</p>
+          )}
+        </div>
+      ) : isLoaded ? (
         <LoadingSpinner />
       ) : products.length > 0 ? (
         <div className="flex flex-row flex-wrap justify-between gap-6">
           <ListOfCards cardType={type.short} products={products} />
         </div>
       ) : (
-        <div>
-          <h2 className="mx-auto w-fit font-semibold text-2xl">Nothing Found</h2>
-          <p className="mx-auto w-fit">You searched for &apos;{searchedValue}&apos;</p>
+        <div className="text-center">
+          <h2 className="font-semibold text-2xl">Nothing Found</h2>
+          <p>You searched for &apos;{searchedValue}&apos;</p>
         </div>
       )}
     </div>
